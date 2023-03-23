78304145e7cec84192d924de6335ecef079c40b7
Tom Wilson <tom@hyper.io>
2023-03-23 14:05:52 +0000

    workshop-1: first query

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,44 @@
+<script>
+  import Arweave from "arweave";
+
+  const options =
+    import.meta.env.MODE === "development"
+      ? { host: "arweave.net", port: 443, protocol: "https" }
+      : {};
+  const arweave = Arweave.init(options);
+
+  const query = `
+query {
+  transactions {
+    edges {
+      node {
+        id
+      }
+    }
+  }
+}
+`;
+
+</script>
+
 <h1 class="my-4 text-4xl text-center">🐘 Permaweb Workshop 🕸</h1>
+{#await arweave.api.post("graphql", { query })}
+  <div
+    class="bg-[#f2f3f4] min-h-screen min-w-screen grid items-center justify-center"
+  >
+    <img
+      alt="loading"
+      src="https://arweave.net/IkMJRqi_0Xx_QhstK4WE3rsQqQxC07n84UagPgqGXfc"
+    />
+  </div>
+{:then result}
+  <div class="mx-2">
+    {#each result.data.data.transactions.edges as edge}
+      <div class="border-2 border-[#ff8500] py-4 my-2 mx-auto text-center">
+        <a href="https://viewblock.io/arweave/tx/{edge.node.id}">
+          {edge.node.id}
+        </a>
+      </div>
+    {/each}
+  </div>
+{/await}
